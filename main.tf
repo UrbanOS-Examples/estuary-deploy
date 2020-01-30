af30780ec8f08599100cafdce30ada08a393e261
marqueeBen Brewer, Your Princi"pal" Artisan/marquee <benjamin.brewer@accenture.com>
2020-01-30 15:24:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,6 +47,7 @@ ingress:
     alb.ingress.kubernetes.io/tags: scos.delete.on.teardown=true
     alb.ingress.kubernetes.io/actions.redirect: '{"Type": "redirect", "RedirectConfig":{"Protocol": "HTTPS", "Port": "443", "StatusCode": "HTTP_301"}}'
     alb.ingress.kubernetes.io/listen-ports: '[{"HTTP": 80}, {"HTTPS": 443}]'
+    alb.ingress.kubernetes.io/load-balancer-attributes: 'idle_timeout.timeout_seconds=600'
   dnsZone: "${data.terraform_remote_state.env_remote_state.internal_dns_zone_name}"
   port: 80
 EOF
