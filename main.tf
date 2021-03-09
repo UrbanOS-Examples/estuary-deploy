7d462c090f88d788a212e4cc056db2b4962162f2
Kyle Smith <kyle.r.smith@accenture.com>
2021-03-09 14:39:00 -0500

    Common#512 run terraform 0.12upgrade on repo

--- main.tf
+++ main.tf
@@ -1,33 +1,33 @@
 provider "aws" {
   version = "~> 3.0"
-  region  = "${var.region}"
+  region  = var.region
 
   assume_role {
-    role_arn = "${var.role_arn}"
+    role_arn = var.role_arn
   }
 }
 
 data "terraform_remote_state" "env_remote_state" {
   backend   = "s3"
-  workspace = "${terraform.workspace}"
+  workspace = terraform.workspace
 
-  config {
-    bucket   = "${var.alm_state_bucket_name}"
+  config = {
+    bucket   = var.alm_state_bucket_name
     key      = "operating-system"
     region   = "us-east-2"
-    role_arn = "${var.alm_role_arn}"
+    role_arn = var.alm_role_arn
   }
 }
 
 resource "random_string" "estuary_lv_salt" {
-  length = 64
-  special = true
+  length           = 64
+  special          = true
   override_special = "/@$#*"
 }
 
 resource "local_file" "kubeconfig" {
   filename = "${path.module}/outputs/kubeconfig"
-  content  = "${data.terraform_remote_state.env_remote_state.eks_cluster_kubeconfig}"
+  content  = data.terraform_remote_state.env_remote_state.outputs.eks_cluster_kubeconfig
 }
 
 # Consume the actions.redirect and listen ports
@@ -40,16 +40,20 @@ ingress:
     kubernetes.io/ingress.class: "alb"
     alb.ingress.kubernetes.io/ssl-policy: "ELBSecurityPolicy-TLS-1-2-2017-01"
     alb.ingress.kubernetes.io/scheme: "${var.is_internal ? "internal" : "internet-facing"}"
-    alb.ingress.kubernetes.io/subnets: "${join(",", data.terraform_remote_state.env_remote_state.public_subnets)}"
-    alb.ingress.kubernetes.io/security-groups: "${data.terraform_remote_state.env_remote_state.allow_all_security_group}"
-    alb.ingress.kubernetes.io/certificate-arn: "${data.terraform_remote_state.env_remote_state.tls_certificate_arn}"
+    alb.ingress.kubernetes.io/subnets: "${join(
+  ",",
+  data.terraform_remote_state.env_remote_state.outputs.public_subnets,
+)}"
+    alb.ingress.kubernetes.io/security-groups: "${data.terraform_remote_state.env_remote_state.outputs.allow_all_security_group}"
+    alb.ingress.kubernetes.io/certificate-arn: "${data.terraform_remote_state.env_remote_state.outputs.tls_certificate_arn}"
     alb.ingress.kubernetes.io/healthcheck-path: "/healthcheck"
     alb.ingress.kubernetes.io/tags: scos.delete.on.teardown=true
     alb.ingress.kubernetes.io/actions.redirect: '{"Type": "redirect", "RedirectConfig":{"Protocol": "HTTPS", "Port": "443", "StatusCode": "HTTP_301"}}'
     alb.ingress.kubernetes.io/listen-ports: '[{"HTTP": 80}, {"HTTPS": 443}]'
-  dnsZone: "${data.terraform_remote_state.env_remote_state.internal_dns_zone_name}"
+  dnsZone: "${data.terraform_remote_state.env_remote_state.outputs.internal_dns_zone_name}"
   port: 80
 EOF
+
 }
 
 resource "null_resource" "helm_deploy" {
@@ -78,12 +82,13 @@ helm upgrade --install estuary scdp/estuary --namespace=streaming-services \
     --values estuary.yaml \
       ${var.extraHelmCommandArgs}
 EOF
+
   }
 
-  triggers {
+  triggers = {
     # Triggers a list of values that, when changed, will cause the resource to be recreated
     # ${uuid()} will always be different thus always executing above local-exec
-    hack_that_always_forces_null_resources_to_execute = "${uuid()}"
+    hack_that_always_forces_null_resources_to_execute = uuid()
   }
 }
 
@@ -121,3 +126,4 @@ variable "extraHelmCommandArgs" {
   description = "Extra command arguments that will be passed to helm upgrade command"
   default     = ""
 }
+
